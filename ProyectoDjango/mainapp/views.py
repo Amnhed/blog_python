06af7e32c4d5fcc0fe12182f2068a24efc66615a
Amnhed <aalhe0.0@gmail.com>
2020-08-16 21:44:15 -0500

    Formulario de registro

--- ProyectoDjango/mainapp/views.py
+++ ProyectoDjango/mainapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -7,3 +8,21 @@ def index(request):
 
 def about(request):
     return render(request,'mainapp/about.html',{'title':'Sobre Nosotros'})
+
+def register_page(request):
+
+    register_form = UserCreationForm()
+
+    if request.method == 'POST':
+        register_form = UserCreationForm(request.POST)
+
+        if register_form.is_valid():
+            register_form.save()
+
+            #va el nombre que asigne en view
+            return redirect('index')
+
+    return render(request,'users/register.html',{
+        'title': 'Registro',
+        'register_form': register_form
+    })
