e9ae50afd98866421fdda97821f930064f76bd9e
Amnhed <aalhe0.0@gmail.com>
2020-08-17 15:43:03 -0500

    Login y validar si el usuario esta logeado

--- ProyectoDjango/mainapp/urls.py
+++ ProyectoDjango/mainapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('inicio/', views.index, name='index'),
     path('registro/', views.register_page, name='register'),
+    path('login/', views.login_page, name='login')
 ]
